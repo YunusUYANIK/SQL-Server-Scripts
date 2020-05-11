b36febbedfa27c548371db21a791dcb0178c4cbb
Yunus <ynsuyanik@gmail.com>
2020-05-11 15:23:38 +0300

    Create Demo - ADR.sql

---
+++ Demo - ADR.sql
@@ -0,0 +1,41 @@
+--ADR rollback scenario
+
+CREATE DATABASE [TestADR]
+ALTER DATABASE [TestADR] SET COMPATIBILITY_LEVEL = 150
+GO
+
+USE [TestADR]
+GO
+
+ALTER DATABASE TestADR SET ACCELERATED_DATABASE_RECOVERY = ON;
+
+SELECT name,is_accelerated_database_recovery_on FROM sys.databases WHERE name='TestADR';
+
+SET STATISTICS IO,TIME ON;
+
+BEGIN TRAN;
+UPDATE Votes SET BountyAmount=10;
+GO
+
+ROLLBACK;
+
+--Without ADR rollback scenario
+
+CREATE DATABASE [TestWithoutADR]
+ALTER DATABASE [TestWithoutADR] SET COMPATIBILITY_LEVEL = 150
+GO
+
+USE [TestWithoutADR]
+GO
+
+ALTER DATABASE TestADR SET ACCELERATED_DATABASE_RECOVERY = OFF;
+
+SELECT name,is_accelerated_database_recovery_on FROM sys.databases WHERE name='TestWithoutADR';
+
+SET STATISTICS IO,TIME ON;
+
+BEGIN TRAN;
+UPDATE Votes SET BountyAmount=10;
+GO
+
+ROLLBACK;
